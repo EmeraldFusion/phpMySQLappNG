b56df7b772e83959c3a41b5793bca38c180ed349
Nick Gilley <95498724+EmeraldFusion@users.noreply.github.com>
2024-10-11 09:52:43 -0500

    Update movieDB.sql

--- mySqlDB/movieDB.sql
+++ mySqlDB/movieDB.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `films`
 --
 
+create DATABASE moviedb;
+use moviedb;
+
 DROP TABLE IF EXISTS `films`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
